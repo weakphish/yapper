7ab03c61684d1f4627c765d1b38236e250810276
Jack Allison <jacallis@cisco.com>
2025-05-20 15:35:12 -0400

    we do a little debugging

--- yap-tui/src/main.rs
+++ yap-tui/src/main.rs
@@ -3,6 +3,7 @@ use std::io;
 use model::{Model, update, view};
 use state::ViewState;
 use util::{handle_event, handle_key};
+use yap_core::block_tree::BlockTree;
 
 mod message;
 mod model;
@@ -11,22 +12,27 @@ mod util;
 
 /// TEA Architecture reference: https://ratatui.rs/concepts/application-patterns/the-elm-architecture/
 /// Runs the application's main loop until the user quits
-fn main() -> io::Result<()> {
-    let mut terminal = ratatui::init();
-    let mut model = Model::new();
-    while model.view_state != ViewState::Exit {
-        // Render current view
-        terminal.draw(|frame| view(&model, frame))?;
+fn main() -> color_eyre::Result<()>{
+    // let mut terminal = ratatui::init();
+ 
+    let block_tree = BlockTree::new("foobar");
+    dbg!(&block_tree);
 
-        // Handle events and map to a Message
-        // TODO: can this be modified to be non-mutable?
-        let mut current_message = handle_event(&model)?;
-
-        // Process updates as long as they return a non-None message
-        while current_message.is_some() {
-            current_message = update(&mut model, current_message.unwrap());
-        }
-    }
-    ratatui::restore();
+    let mut model = Model::new(block_tree);
+    dbg!(&model);
+    // while model.view_state != ViewState::Exit {
+    //     // Render current view
+    //     terminal.draw(|frame| view(&model, frame))?;
+    //
+    //     // Handle events and map to a Message
+    //     // TODO: can this be modified to be non-mutable?
+    //     let mut current_message = handle_event(&model)?;
+    //
+    //     // Process updates as long as they return a non-None message
+    //     while current_message.is_some() {
+    //         current_message = update(&mut model, current_message.unwrap());
+    //     }
+    // }
+    // ratatui::restore();
     Ok(())
 }
--- yap-tui/src/model.rs
+++ yap-tui/src/model.rs
@@ -1,23 +1,31 @@
+use std::rc::Rc;
+
 use crate::message::Message;
 use crate::state::ViewState;
+use ratatui::layout::{Constraint, Direction, Layout, Rect};
 use ratatui::style::{Color, Style};
 use ratatui::widgets::{BorderType, Borders, Paragraph};
 use ratatui::Frame;
-use yap_core::block::Block;
+use yap_core::block::{self, Block};
+use yap_core::block_tree::BlockTree;
 
 #[derive(Debug)]
 pub struct Model<'a> {
     pub view_state: ViewState,
+    pub block_tree: BlockTree,
     pub current_block: Option<&'a Block>,
     pub exit: bool,
 }
 
 impl <'a> Model<'a> {
     /// Create a new model instance defaulting to the daily page view and no current block
-    /// selection
-    pub fn new() -> Self {
+    /// selection.
+    /// TODO: Model should own the block tree, right? what about when we switch pages?
+    /// thought: the block tree is app-global, each root is a page/tag?
+    pub fn new(block_tree: BlockTree) -> Self {
         Model{
             view_state: ViewState::DailyPage,
+            block_tree, 
             current_block: None,
             exit: false
         }
@@ -26,16 +34,32 @@ impl <'a> Model<'a> {
 
 /// Take the model and a frame and render the view with the given state
 pub fn view(model: &Model, frame: &mut Frame) {
-    // TODO: for now, just render a single block
-    let p = Paragraph::new("Hello, World!")
-    .style(Style::default().fg(Color::Yellow))
-    .block(
-        ratatui::widgets::Block::default()
-            .borders(Borders::ALL)
-            .title("Title")
-            .border_type(BorderType::Rounded)
-    );
-    frame.render_widget(p, frame.area());
+    let paragraph_blocks = model.block_tree.get_tree_pre_order().iter().map(|id| {
+        let block = model.block_tree.get_block_by_id(id).unwrap();
+        let content = block.content();
+        let p = Paragraph::new(content.to_string().clone())
+        .style(Style::default().fg(Color::Yellow))
+        .block(
+            ratatui::widgets::Block::default()
+                .borders(Borders::ALL)
+                .title("Title")
+                .border_type(BorderType::Rounded)
+        );
+        frame.render_widget(p, frame.area());
+    });
+}
+
+/// Get layout for a page based on how many blocks there are
+fn get_layout_based_on_blocks(block_count: usize, f: &Frame) -> Rc<[Rect]> {
+    let block_percentage = if block_count > 50 { 80 } else { 50 };
+
+    Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([
+            Constraint::Percentage(block_percentage),
+            Constraint::Percentage(100 - block_percentage),
+        ])
+        .split(f.area())
 }
 
 /// Take the model and update it based on the message received
